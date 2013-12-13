be315ab661b30188f89bbc230ea6bb46a32a1ef6
Philip John <phil@philipjohn.co.uk>
2013-12-13 18:57:19 +0000

    Initial commit

---
+++ allow-javascript-in-text-widgets.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: Allow Javascript in Text Widgets
+Plugin URI: http://philipjohn.co.uk/#pj-better-multisite-text-widget
+Description: Replaces the default text widget with one that allows Javascript so you can do basic things like add Google Ads to your sidebar without using other plugins.
+Version: 0.2
+Author: Philip John
+Author URI: http://philipjohn.co.uk
+License: GPL2
+Network: true
+*/
+
+/**
+ * Text widget class
+ */
+class WP_Widget_Text_With_JS extends WP_Widget {
+
+	function __construct() {
+		$widget_ops = array('classname' => 'widget_text', 'description' => __('Arbitrary text or HTML'));
+		$control_ops = array('width' => 400, 'height' => 350);
+		parent::__construct('text', __('Text'), $widget_ops, $control_ops);
+	}
+
+	function widget( $args, $instance ) {
+		extract($args);
+		$title = apply_filters( 'widget_title', empty($instance['title']) ? '' : $instance['title'], $instance, $this->id_base);
+		$text = apply_filters( 'widget_text', $instance['text'], $instance );
+		echo $before_widget;
+		if ( !empty( $title ) ) { echo $before_title . $title . $after_title; } ?>
+			<div class="textwidget"><?php echo $instance['filter'] ? wpautop($text) : $text; ?></div>
+		<?php
+		echo $after_widget;
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['text'] =  $new_instance['text'];
+		$instance['filter'] = isset($new_instance['filter']);
+		return $instance;
+	}
+
+	function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'text' => '' ) );
+		$title = strip_tags($instance['title']);
+		$text = esc_textarea($instance['text']);
+?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
+
+		<textarea class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id('text'); ?>" name="<?php echo $this->get_field_name('text'); ?>"><?php echo $text; ?></textarea>
+
+		<p><input id="<?php echo $this->get_field_id('filter'); ?>" name="<?php echo $this->get_field_name('filter'); ?>" type="checkbox" <?php checked(isset($instance['filter']) ? $instance['filter'] : 0); ?> />&nbsp;<label for="<?php echo $this->get_field_id('filter'); ?>"><?php _e('Automatically add paragraphs'); ?></label></p>
+<?php
+	}
+}
+function wp_widget_text_with_js_init(){
+	unregister_widget('WP_Widget_Text');
+	register_widget('WP_Widget_Text_With_JS');
+}
+add_action('widgets_init', 'wp_widget_text_with_js_init', 1);
+?>
---
+++ readme.txt
@@ -0,0 +1,19 @@
+=== Allow Javascript in Text Widgets ===
+Contributors: philipjohn
+Tags: widget, javascript, text widget, kses, filter
+Requires at least: 3.2.1
+Tested up to: 3.2
+
+Replaces the default text widget with one that allows Javascript so you can do basic things like add Google Ads to your sidebar without using other plugins.
+
+== Description ==
+
+Replaces the default text widget with one that allows Javascript so you can do basic things like add Google Ads to your sidebar without using other plugins.
+
+Important: It's only intended for Multisite. If you use it in standard WordPress and it doesn't work or breaks something don't expect any sympathy.
+
+== Installation ==
+
+1. Upload to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in Network Admin
+
