09642840f75306b0bf170b9ce4e1c272b77724ce
Philip John <phil@philipjohn.co.uk>
2015-11-26 12:53:21 +0000

    Formatting tweaks

--- allow-javascript-in-text-widgets.php
+++ allow-javascript-in-text-widgets.php
@@ -16,48 +16,63 @@ Text Domain: allow-javascript-in-text-widgets
  */
 class WP_Widget_Text_With_JS extends WP_Widget {
 
-	function __construct() {
-		$widget_ops = array('classname' => 'widget_text', 'description' => __('Arbitrary text or HTML', 'allow-javascript-in-text-widgets'));
-		$control_ops = array('width' => 400, 'height' => 350);
-		parent::__construct('text', __('Text', 'allow-javascript-in-text-widgets'), $widget_ops, $control_ops);
-	}
+    function __construct() {
+        $widget_ops = array(
+			'classname' => 'widget_text',
+			'description' => __('Arbitrary text or HTML', 'allow-javascript-in-text-widgets')
+        );
+        $control_ops = array(
+			'width' => 400,
+	        'height' => 350
+        );
+        parent::__construct(
+            'text',
+            __('Text', 'allow-javascript-in-text-widgets'),
+            $widget_ops,
+            $control_ops
+        );
+    }
 
-	function widget( $args, $instance ) {
-		extract($args);
-		$title = apply_filters( 'widget_title', empty($instance['title']) ? '' : $instance['title'], $instance, $this->id_base);
-		$text = apply_filters( 'widget_text', $instance['text'], $instance );
-		echo $before_widget;
-		if ( !empty( $title ) ) { echo $before_title . $title . $after_title; } ?>
+    function widget( $args, $instance ) {
+        extract($args);
+
+        $title = apply_filters('widget_title', empty($instance['title']) ? '' : $instance['title'], $instance, $this->id_base);
+        $text = apply_filters('widget_text', $instance['text'], $instance);
+
+        echo $before_widget;
+        if (!empty($title) ) {
+			echo $before_title . $title . $after_title;
+        }
+		?>
 			<div class="textwidget"><?php echo $instance['filter'] ? wpautop($text) : $text; ?></div>
-		<?php
-		echo $after_widget;
-	}
+        <?php
+        echo $after_widget;
+    }
 
-	function update( $new_instance, $old_instance ) {
-		$instance = $old_instance;
-		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['text'] =  $new_instance['text'];
-		$instance['filter'] = isset($new_instance['filter']);
-		return $instance;
-	}
+    function update( $new_instance, $old_instance ) {
+        $instance = $old_instance;
+        $instance['title'] = strip_tags($new_instance['title']);
+        $instance['text'] =  $new_instance['text'];
+        $instance['filter'] = isset($new_instance['filter']);
+        return $instance;
+    }
 
-	function form( $instance ) {
-		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'text' => '' ) );
-		$title = strip_tags($instance['title']);
-		$text = esc_textarea($instance['text']);
-?>
-		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'allow-javascript-in-text-widgets'); ?></label>
+    function form( $instance ) {
+        $instance = wp_parse_args((array) $instance, array( 'title' => '', 'text' => '' ));
+        $title = strip_tags($instance['title']);
+        $text = esc_textarea($instance['text']);
+    ?>
+     <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'allow-javascript-in-text-widgets'); ?></label>
 		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
 
 		<textarea class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id('text'); ?>" name="<?php echo $this->get_field_name('text'); ?>"><?php echo $text; ?></textarea>
 
 		<p><input id="<?php echo $this->get_field_id('filter'); ?>" name="<?php echo $this->get_field_name('filter'); ?>" type="checkbox" <?php checked(isset($instance['filter']) ? $instance['filter'] : 0); ?> />&nbsp;<label for="<?php echo $this->get_field_id('filter'); ?>"><?php _e('Automatically add paragraphs', 'allow-javascript-in-text-widgets'); ?></label></p>
-<?php
-	}
+	<?php
+    }
 }
-function wp_widget_text_with_js_init(){
-	unregister_widget('WP_Widget_Text');
-	register_widget('WP_Widget_Text_With_JS');
+function wp_widget_text_with_js_init() {
+    unregister_widget('WP_Widget_Text');
+    register_widget('WP_Widget_Text_With_JS');
 }
 add_action('widgets_init', 'wp_widget_text_with_js_init', 1);
-?>
